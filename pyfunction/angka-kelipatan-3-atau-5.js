73c925dcd8b64809f55fe42b52676d41e78c848f
GregoriusDavin <102125620+GregoriusDavin@users.noreply.github.com>
2023-08-22 10:41:42 +0700

    angka kelipatan 3 atau 5 js

---
+++ pyfunction/angka-kelipatan-3-atau-5.js
@@ -0,0 +1,14 @@
+function solution(number){
+    if (number < 0) {
+      return 0;
+   }   
+    let x = 0;
+    for (let i = 3; i < number; i++){
+      if (i % 3 === 0 || i % 5 === 0) {
+        x += i;
+      }
+    }
+    return x;
+    }
+
+console.log(solution(1234));

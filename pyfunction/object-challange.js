37cf459ae32426d61deebfa9eda99b1ec9be505a
GregoriusDavin <102125620+GregoriusDavin@users.noreply.github.com>
2023-09-01 21:14:00 +0700

    fcc object challenge

---
+++ pyfunction/object-challange.js
@@ -0,0 +1,36 @@
+
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  function updateRecords(records, id, prop, value) {
+    if (value === ""){
+      delete records[id][prop];
+    } else if (prop != "tracks"){
+        records[id][prop] = value;
+    } else if (prop === "tracks"){
+        if (records[id].hasOwnProperty("tracks") === false){
+            records[id][prop] = [];
+        }
+        records[id][prop].push(value)
+    }
+    return records;
+  }
+  
+ console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));

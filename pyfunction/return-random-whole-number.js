b345942e09643cdbb9de7f7a968fa86f57114cdf
GregoriusDavin <102125620+GregoriusDavin@users.noreply.github.com>
2023-09-03 19:55:29 +0700

    added min and max number

--- pyfunction/return-random-whole-number.js
+++ pyfunction/return-random-whole-number.js
@@ -2,3 +2,9 @@ function randomWholeNum() {
     return Math.floor(Math.random() * 10);
   }
   console.log(randomWholeNum)
+
+  function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+  
+  console.log(randomRange(1, 6));

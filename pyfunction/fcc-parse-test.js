c46849897ab505c620ffe90b5bbba1b7415ef7dc
GregoriusDavin <102125620+GregoriusDavin@users.noreply.github.com>
2023-09-03 20:03:42 +0700

    added binary to number

--- pyfunction/fcc-parse-test.js
+++ pyfunction/fcc-parse-test.js
@@ -5,3 +5,11 @@ function convertToInteger(str) {
   
   convertToInteger("56");
   console.log(convertToInteger("56"))
+
+function convertToBinary(str) {
+const a = parseInt(str, 2)
+return a;
+}
+
+convertToInteger("10011");
+console.log(convertToInteger("10011"))

e3c3f65c1e727589b1f55657bbb4fea4f2f1d9f6
GregoriusDavin <102125620+GregoriusDavin@users.noreply.github.com>
2023-09-04 20:41:18 +0700

    testbench for fulladder

---
+++ edaplayground/fulladder/testbench.sv
@@ -0,0 +1,38 @@
+module testbench;
+
+reg A, B, Cin;
+wire Sum, Cout;
+
+  FullAdder dut ( //insialisasi modul FullAdder
+  .A(A),
+  .B(B),
+  .Cin(Cin),
+  .Sum(Sum),
+  .Cout(Cout)
+);
+
+initial begin
+  $display("A B Cin | Sum Cout");
+  $monitor("%b %b  %b  |  %b   %b", A, B, Cin, Sum, Cout);
+  
+  A = 0; B = 0; Cin = 0; #10;
+  A = 0; B = 0; Cin = 1; #10;
+  A = 0; B = 1; Cin = 0; #10;
+  A = 0; B = 1; Cin = 1; #10;
+  A = 1; B = 0; Cin = 0; #10;
+  A = 1; B = 0; Cin = 1; #10;
+  A = 1; B = 1; Cin = 0; #10;
+  A = 1; B = 1; Cin = 1; #10;
+  
+  $finish;
+end
+
+initial
+    begin
+      //dump vars
+      $display("\n -----------------DUMP--------------------------------------\mn");
+      $dumpfile("dump.vcd");
+      $dumpvars(1);
+    end
+ 
+endmodule
